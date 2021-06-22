4d41b7c3d589e4c28c9428a6430bddaf60777ad4
Dmitriy <xyzmail@yandex.ru>
2021-06-22 23:02:21 +0400

    added close webDriver

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,7 +2,10 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 
 public class TestBase {
@@ -16,6 +19,9 @@ public class TestBase {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
-
+    @AfterEach
+    public void tearDown() {
+        closeWebDriver();
+    }
 
 }
