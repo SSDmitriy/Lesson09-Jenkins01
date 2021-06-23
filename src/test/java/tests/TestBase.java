0c00c355049b30dcced4233ed9c779c04c8258a5
Dmitriy <xyzmail@yandex.ru>
2021-06-23 22:16:46 +0400

    added Allure

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,6 +2,9 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -14,6 +17,7 @@ public class TestBase {
 
     @BeforeAll
     static void setup() {
+        SelenideLogger.addListener("AllureListener", new AllureSelenide());
         Configuration.startMaximized = true;
         //fof remote run autotest
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
@@ -21,6 +25,10 @@ public class TestBase {
 
     @AfterEach
     public void tearDown() {
+        Attach.screenshotAs("ScreenShot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+
         closeWebDriver();
     }
 
